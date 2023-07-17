dbe219aa238d5a488a9a7412d0bdbe2074240a4d
Marino Caceres <marinoe2005@gmail.com>
2023-07-17 15:02:26 -0700

    Initial Stage

---
+++ exercise-1.py
@@ -0,0 +1,56 @@
+z = 78
+print(78)
+print("apple")
+
+a = 3
+print(a)
+a = 7
+print(a)
+
+b = 4
+c = a*4
+print(b+c)
+print(b*c)
+
+c = c/a
+print(c)
+
+d = 0.1+0.2
+print(d)
+d = d*10
+print(d)
+
+f = 4+8/2+1
+print(f)
+
+s = "anaconda"
+c = 2
+d = 3
+print (s)
+print(s[2])
+print(s[c])
+print(s[d])
+print(s[2])
+print(s[-a])
+
+a**2
+print(a)
+print(a**2)
+
+b = "not"
+print(b+s)
+
+b = s[4]
+print(b)
+print(b+s)
+
+e = 4**(0.5)
+print(e)
+
+g = e+2
+print(e/g)
+
+h = 23%5
+print(h)
+print(h%2)
+print(h+1)
---
+++ quadratic-solver.py
@@ -0,0 +1,14 @@
+print("I am a Quadratic Equation Solver")
+print("ax^2 + bx + c = 0")
+
+a = float(input("What is a: "))
+b = float(input("What is b: "))
+c = float(input("What is c: "))
+
+d=(b**2-4*a*c)**(0.5)
+
+x1=(-b + d)/2*a
+x2=(-b - d)/2*a
+
+print("The first root is:",x1)
+print("The second root is:",x2)
