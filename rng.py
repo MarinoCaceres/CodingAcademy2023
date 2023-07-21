b317b4b6ddec36ff97ae634999e2aa790662da46
Marino Caceres <marinoe2005@gmail.com>
2023-07-21 10:11:01 -0700

    Restart and RNG Range implementation

--- rng.py
+++ rng.py
@@ -9,19 +9,28 @@ import random
 
 choice = ""
 guess = ""
+rng = random.randrange(100)
+guesses = 0
+cheat = False
 while choice != "n" and guess != "q":
-    rng = random.randrange(100)
-    guesses = 0
-    cheat = False
-
-    while guess != rng and guess != "q":
+    while guess != rng and guess != "q" or guess == "r" or guess == "s":
         guess = (input("Guess what my secret number is: "))
         
+        if guess == "s":
+            set = int(input("What number should be the range of my secret number: "))
+            rng = random.randrange(set)
+            cheat = True
+
+        if guess == "r":
+            rng = random.randrange(100)
+            guesses = 0
+            cheat = False
+
         if guess == "z":
             print("Dont tell anyone, but his number is",rng)
             cheat = True
         
-        elif guess != "q":
+        elif guess != "q" and guess != "r" and guess != "s":
             guess = int(guess)
             guesses += 1
 
@@ -31,9 +40,14 @@ while choice != "n" and guess != "q":
                 print("Wrong lmao: My number is higher than",guess)
             
     if cheat == True:
+        guess = ""
+        rng = random.randrange(100)
         print("Did you cheat or something? It took you",guesses,"guesses.")
         choice = input("Do you wish to play again? (y/n)")
-    elif guess != "q":
+
+    elif guess != "q" and guess != "r" and guess != "s":
+        guess = ""
+        rng = random.randrange(100)
         print ("Nicely done mate, very cool. It took you",guesses,"guesses.")
         choice = input("Do you wish to play again? (y/n)")
 
